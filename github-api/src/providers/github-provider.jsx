364beeca5f87561a770ddfdd6352c75dadbc74b1
Gabriel Lumertz <glumertz10@gmail.com>
2022-05-22 14:22:41 -0300

    adicionado pesquisa de usuário

--- github-api/src/providers/github-provider.jsx
+++ github-api/src/providers/github-provider.jsx
@@ -1,18 +1,21 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useCallback, useState } from "react";
+import api from './../services/api'
 
 export const GithubContext = createContext({
+    loading: false,
     user: {},
     repositories: [],
     starred: []
 })
 
-const GithubProvider = ({children}) => {
-    
-    const [ githubState, setGithubState ] = useState({
+const GithubProvider = ({ children }) => {
+
+    const [githubState, setGithubState] = useState({
+        loading: false,
         user: {
             login: undefined,
             name: undefined,
-            publicUrl: undefined,
+            html_url: undefined,
             blog: undefined,
             company: undefined,
             location: undefined,
@@ -25,11 +28,32 @@ const GithubProvider = ({children}) => {
         starred: [],
     })
 
-    const contextValue = {
-        githubState,
+    const getUser = (username) => {
+        api.get(`users/${username}`).then(({ data: { user }}) => {
+            setGithubState(prevState => ({
+                ...prevState,
+                user: {
+                    login: user.login,
+                    name: user.name,
+                    html_url: user.html_url,
+                    blog: user.blog,
+                    company: user.company,
+                    location: user.location,
+                    followers: user.followers,
+                    following: user.following,
+                    public_gists: user.public_gists,
+                    public_repos: user.public_repos,
+                }
+            }))
+        })
     }
 
-    return(
+    const contextValue = {
+        githubState,
+        getUser: useCallback((username) => getUser(username), [])
+    }
+
+    return (
         <GithubContext.Provider value={contextValue}>
             {children}
         </GithubContext.Provider>
